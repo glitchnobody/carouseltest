cdda6a766c3138abdbd4e632f47733cc7bb2e70d
glitchnobody <58382686+glitchnobody@users.noreply.github.com>
2023-07-31 15:58:40 +0530

    cdcd

---
+++ src/pages/data.js
@@ -0,0 +1,42 @@
+export const content = {
+  page1: {
+    title: "page 1",
+    subtitle: "I'm a subtitle",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris molestie odio a tincidunt dapibus. Aenean scelerisque ex feugiat accumsan tincidunt. Maecenas a ultrices libero, nec cursus mauris. Proin imperdiet ligula quis facilisis luctus. Suspendisse ultrices tellus diam, et condimentum nulla malesuada sit amet. Quisque a tellus et erat lobortis rhoncus. Duis dui dui, semper in vulputate vel, consectetur ac mauris. Quisque quis est pretium, faucibus est in, pretium diam. Nunc iaculis congue mauris nec mollis.   ",
+  },
+  page2: {
+    title: "page 2",
+    subtitle: "I'm a subtitle",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris molestie odio a tincidunt dapibus. Aenean scelerisque ex feugiat accumsan tincidunt. Maecenas a ultrices libero, nec cursus mauris. Proin imperdiet ligula quis facilisis luctus. Suspendisse ultrices tellus diam, et condimentum nulla malesuada sit amet. Quisque a tellus et erat lobortis rhoncus. Duis dui dui, semper in vulputate vel, consectetur ac mauris. Quisque quis est pretium, faucibus est in, pretium diam. Nunc iaculis congue mauris nec mollis.   ",
+  },
+  page3: {
+    title: "page 3",
+    subtitle: "I'm a subtitle",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris molestie odio a tincidunt dapibus. Aenean scelerisque ex feugiat accumsan tincidunt. Maecenas a ultrices libero, nec cursus mauris. Proin imperdiet ligula quis facilisis luctus. Suspendisse ultrices tellus diam, et condimentum nulla malesuada sit amet. Quisque a tellus et erat lobortis rhoncus. Duis dui dui, semper in vulputate vel, consectetur ac mauris. Quisque quis est pretium, faucibus est in, pretium diam. Nunc iaculis congue mauris nec mollis.   ",
+  },
+  page4: {
+    title: "page 4",
+    subtitle: "I'm a subtitle",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris molestie odio a tincidunt dapibus. Aenean scelerisque ex feugiat accumsan tincidunt. Maecenas a ultrices libero, nec cursus mauris. Proin imperdiet ligula quis facilisis luctus. Suspendisse ultrices tellus diam, et condimentum nulla malesuada sit amet. Quisque a tellus et erat lobortis rhoncus. Duis dui dui, semper in vulputate vel, consectetur ac mauris. Quisque quis est pretium, faucibus est in, pretium diam. Nunc iaculis congue mauris nec mollis.   ",
+  },
+  page5: {
+    title: "page 5",
+    subtitle: "I'm a subtitle",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris molestie odio a tincidunt dapibus. Aenean scelerisque ex feugiat accumsan tincidunt. Maecenas a ultrices libero, nec cursus mauris. Proin imperdiet ligula quis facilisis luctus. Suspendisse ultrices tellus diam, et condimentum nulla malesuada sit amet. Quisque a tellus et erat lobortis rhoncus. Duis dui dui, semper in vulputate vel, consectetur ac mauris. Quisque quis est pretium, faucibus est in, pretium diam. Nunc iaculis congue mauris nec mollis.   ",
+  },
+  page6: {
+    title: "page 6",
+    subtitle: "I'm a subtitle",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris molestie odio a tincidunt dapibus. Aenean scelerisque ex feugiat accumsan tincidunt. Maecenas a ultrices libero, nec cursus mauris. Proin imperdiet ligula quis facilisis luctus. Suspendisse ultrices tellus diam, et condimentum nulla malesuada sit amet. Quisque a tellus et erat lobortis rhoncus. Duis dui dui, semper in vulputate vel, consectetur ac mauris. Quisque quis est pretium, faucibus est in, pretium diam. Nunc iaculis congue mauris nec mollis.   ",
+  },
+  page7: {
+    title: "page 7",
+    subtitle: "I'm a subtitle",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris molestie odio a tincidunt dapibus. Aenean scelerisque ex feugiat accumsan tincidunt. Maecenas a ultrices libero, nec cursus mauris. Proin imperdiet ligula quis facilisis luctus. Suspendisse ultrices tellus diam, et condimentum nulla malesuada sit amet. Quisque a tellus et erat lobortis rhoncus. Duis dui dui, semper in vulputate vel, consectetur ac mauris. Quisque quis est pretium, faucibus est in, pretium diam. Nunc iaculis congue mauris nec mollis.   ",
+  },
+  page8: {
+    title: "page 8",
+    subtitle: "I'm a subtitle",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris molestie odio a tincidunt dapibus. Aenean scelerisque ex feugiat accumsan tincidunt. Maecenas a ultrices libero, nec cursus mauris. Proin imperdiet ligula quis facilisis luctus. Suspendisse ultrices tellus diam, et condimentum nulla malesuada sit amet. Quisque a tellus et erat lobortis rhoncus. Duis dui dui, semper in vulputate vel, consectetur ac mauris. Quisque quis est pretium, faucibus est in, pretium diam. Nunc iaculis congue mauris nec mollis.   ",
+  },
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,68 +1,81 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import { useState, useEffect } from "react";
-
+import { content } from "./data";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const content = {
-    page1: {
-      title: "page 1",
-      subtitle: "I'm a subtitle",
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris molestie odio a tincidunt dapibus. Aenean scelerisque ex feugiat accumsan tincidunt. Maecenas a ultrices libero, nec cursus mauris. Proin imperdiet ligula quis facilisis luctus. Suspendisse ultrices tellus diam, et condimentum nulla malesuada sit amet. Quisque a tellus et erat lobortis rhoncus. Duis dui dui, semper in vulputate vel, consectetur ac mauris. Quisque quis est pretium, faucibus est in, pretium diam. Nunc iaculis congue mauris nec mollis.   ",
-    },
-    page2: {
-      title: "page 2",
-      subtitle: "I'm a subtitle",
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris molestie odio a tincidunt dapibus. Aenean scelerisque ex feugiat accumsan tincidunt. Maecenas a ultrices libero, nec cursus mauris. Proin imperdiet ligula quis facilisis luctus. Suspendisse ultrices tellus diam, et condimentum nulla malesuada sit amet. Quisque a tellus et erat lobortis rhoncus. Duis dui dui, semper in vulputate vel, consectetur ac mauris. Quisque quis est pretium, faucibus est in, pretium diam. Nunc iaculis congue mauris nec mollis.   ",
-    },
-    page3: {
-      title: "page 3",
-      subtitle: "I'm a subtitle",
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris molestie odio a tincidunt dapibus. Aenean scelerisque ex feugiat accumsan tincidunt. Maecenas a ultrices libero, nec cursus mauris. Proin imperdiet ligula quis facilisis luctus. Suspendisse ultrices tellus diam, et condimentum nulla malesuada sit amet. Quisque a tellus et erat lobortis rhoncus. Duis dui dui, semper in vulputate vel, consectetur ac mauris. Quisque quis est pretium, faucibus est in, pretium diam. Nunc iaculis congue mauris nec mollis.   ",
-    },
-    page4: {
-      title: "page 4",
-      subtitle: "I'm a subtitle",
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris molestie odio a tincidunt dapibus. Aenean scelerisque ex feugiat accumsan tincidunt. Maecenas a ultrices libero, nec cursus mauris. Proin imperdiet ligula quis facilisis luctus. Suspendisse ultrices tellus diam, et condimentum nulla malesuada sit amet. Quisque a tellus et erat lobortis rhoncus. Duis dui dui, semper in vulputate vel, consectetur ac mauris. Quisque quis est pretium, faucibus est in, pretium diam. Nunc iaculis congue mauris nec mollis.   ",
-    },
-    page5: {
-      title: "page 5",
-      subtitle: "I'm a subtitle",
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris molestie odio a tincidunt dapibus. Aenean scelerisque ex feugiat accumsan tincidunt. Maecenas a ultrices libero, nec cursus mauris. Proin imperdiet ligula quis facilisis luctus. Suspendisse ultrices tellus diam, et condimentum nulla malesuada sit amet. Quisque a tellus et erat lobortis rhoncus. Duis dui dui, semper in vulputate vel, consectetur ac mauris. Quisque quis est pretium, faucibus est in, pretium diam. Nunc iaculis congue mauris nec mollis.   ",
-    },
-    page6: {
-      title: "page 6",
-      subtitle: "I'm a subtitle",
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris molestie odio a tincidunt dapibus. Aenean scelerisque ex feugiat accumsan tincidunt. Maecenas a ultrices libero, nec cursus mauris. Proin imperdiet ligula quis facilisis luctus. Suspendisse ultrices tellus diam, et condimentum nulla malesuada sit amet. Quisque a tellus et erat lobortis rhoncus. Duis dui dui, semper in vulputate vel, consectetur ac mauris. Quisque quis est pretium, faucibus est in, pretium diam. Nunc iaculis congue mauris nec mollis.   ",
-    },
-    page7: {
-      title: "page 7",
-      subtitle: "I'm a subtitle",
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris molestie odio a tincidunt dapibus. Aenean scelerisque ex feugiat accumsan tincidunt. Maecenas a ultrices libero, nec cursus mauris. Proin imperdiet ligula quis facilisis luctus. Suspendisse ultrices tellus diam, et condimentum nulla malesuada sit amet. Quisque a tellus et erat lobortis rhoncus. Duis dui dui, semper in vulputate vel, consectetur ac mauris. Quisque quis est pretium, faucibus est in, pretium diam. Nunc iaculis congue mauris nec mollis.   ",
-    },
-    page8: {
-      title: "page 8",
-      subtitle: "I'm a subtitle",
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris molestie odio a tincidunt dapibus. Aenean scelerisque ex feugiat accumsan tincidunt. Maecenas a ultrices libero, nec cursus mauris. Proin imperdiet ligula quis facilisis luctus. Suspendisse ultrices tellus diam, et condimentum nulla malesuada sit amet. Quisque a tellus et erat lobortis rhoncus. Duis dui dui, semper in vulputate vel, consectetur ac mauris. Quisque quis est pretium, faucibus est in, pretium diam. Nunc iaculis congue mauris nec mollis.   ",
-    },
+  const keys = Object.keys(content);
+  const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
+
+  const moveRight = () => {
+    setCurrentSlideIndex((prevIndex) => (prevIndex + 1) % keys.length);
   };
 
-  // carousel of pages
+  const moveLeft = () => {
+    setCurrentSlideIndex(
+      (prevIndex) => (prevIndex - 1 + keys.length) % keys.length
+    );
+  };
 
-  const [pageContent, setPageContent] = useState(content);
+  const isPrevButtonDisabled = currentSlideIndex === 0;
+  const isNextButtonDisabled = currentSlideIndex === keys.length - 1;
 
   return (
-    <main>
-      {Object.keys(pageContent).map((page, index) => {
-        return (
-          <div key={index}>
-            <h1>{pageContent[page].title}</h1>
-            <h2>{pageContent[page].subtitle}</h2>
-            <p>{pageContent[page].text}</p>
+    <div>
+      <div className="carousel-container">
+        <div className="left-container">
+          {!isPrevButtonDisabled && (
+            <div className="carousel-slide slide-left" id="slide-left">
+              <h2>
+                {content[keys[(currentSlideIndex - 1) % keys.length]].title}
+              </h2>
+              <h3>
+                {content[keys[(currentSlideIndex - 1) % keys.length]].subtitle}
+              </h3>
+              <p>{content[keys[(currentSlideIndex - 1) % keys.length]].text}</p>
+            </div>
+          )}
+        </div>
+        <div className="middle-container">
+          <div className="carousel-slide slide-middle" id="slide-middle">
+            <h2>{content[keys[currentSlideIndex]].title}</h2>
+            <h3>{content[keys[currentSlideIndex]].subtitle}</h3>
+            <p>{content[keys[currentSlideIndex]].text}</p>
           </div>
-        );
-      })}
-    </main>
+        </div>
+
+        <div className="right-container">
+          {!isNextButtonDisabled && (
+            <div className="carousel-slide slide-right-1">
+              <h2>
+                {content[keys[(currentSlideIndex + 1) % keys.length]].title}
+              </h2>
+              <h3>
+                {content[keys[(currentSlideIndex + 1) % keys.length]].subtitle}
+              </h3>
+              <p>{content[keys[(currentSlideIndex + 1) % keys.length]].text}</p>
+            </div>
+          )}
+          {!isNextButtonDisabled && currentSlideIndex < keys.length - 2 && (
+            <div className="carousel-slide slide-right-2">
+              <h2>
+                {content[keys[(currentSlideIndex + 2) % keys.length]].title}
+              </h2>
+              <h3>
+                {content[keys[(currentSlideIndex + 2) % keys.length]].subtitle}
+              </h3>
+              <p>{content[keys[(currentSlideIndex + 2) % keys.length]].text}</p>
+            </div>
+          )}
+        </div>
+      </div>
+      <button onClick={moveLeft} disabled={isPrevButtonDisabled}>
+        Previous
+      </button>
+      <button onClick={moveRight} disabled={isNextButtonDisabled}>
+        Next
+      </button>
+    </div>
   );
 }
